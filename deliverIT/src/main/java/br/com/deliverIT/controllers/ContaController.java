7617d31208e207978cc2b4ca3c7b81ca596158bd
miolo <ggrbedin@gmail.com>
2018-10-31 12:07:28 -0200

    Persistencia

--- deliverIT/src/main/java/br/com/deliverIT/controllers/ContaController.java
+++ deliverIT/src/main/java/br/com/deliverIT/controllers/ContaController.java
@@ -1,20 +1,34 @@
 package br.com.deliverIT.controllers;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.deliverIT.model.Conta;
+import br.com.deliverIT.repositories.ContaRepository;
 
 @RestController
 public class ContaController {
 	
+	@Autowired
+	private ContaRepository contaRepo;
+	
 	private final AtomicLong idGenerator = new AtomicLong();
 	
-	@RequestMapping("/conta")
-	public Conta conta(@RequestParam(value="nome") String nome) {
-		return new Conta(idGenerator.incrementAndGet(), nome);
+	@RequestMapping(value = "/conta", method={RequestMethod.POST,RequestMethod.GET})
+	public List<Conta> conta(@RequestParam("nome") String nome, 
+						@RequestParam("valor") String valor, 
+						@RequestParam("dataVencimento") String dataVencimento, 
+						@RequestParam("dataPagamento") String dataPagamento) {
+		if(nome != null) {
+			Conta conta = new Conta(nome, valor, dataVencimento, dataPagamento);
+			contaRepo.save(conta);
+		}
+		return contaRepo.findAll();
 	}
 }
--- deliverIT/src/main/java/br/com/deliverIT/model/Conta.java
+++ deliverIT/src/main/java/br/com/deliverIT/model/Conta.java
@@ -1,17 +1,37 @@
 package br.com.deliverIT.model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Conta {
-	private long id;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	private String nome;
 	private Double valorOriginal;
 	private LocalDate dataVencimento;
 	private LocalDate dataPagamento;
 	
+	public Conta() {}
+	
 	public Conta(long id, String nome) {
 		this.id = id;
 		this.nome = nome;
+	}
+	
+	public Conta(String nome, String valorOriginal, String dataVencimento, String dataPagamento) {
+		this.nome = nome;
+		this.valorOriginal = Double.parseDouble(valorOriginal.replace(",", "."));
+		this.dataVencimento = LocalDate.parse(dataVencimento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		this.dataPagamento = LocalDate.parse(dataPagamento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 	}
 
 	public String getNome() {
@@ -20,7 +40,7 @@ public class Conta {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 	
